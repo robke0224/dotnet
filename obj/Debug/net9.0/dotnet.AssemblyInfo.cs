07bab68405916e733ba2936a1c0d89f1ee5cb4e3
Roberta Jurpalyte <robertajurpalyte@Robertas-MacBook-Air.local>
2025-10-22 02:09:53 +0300

    add .gitignore and remove build artifacts

--- obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fed6113ae2f88c5a4db894f8ce1baafb136112d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
