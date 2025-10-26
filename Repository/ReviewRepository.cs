d891462e1ce284d931bebba3edf9fa4b294749db
Roberta Jurpalyte <robertajurpalyte@Robertas-MacBook-Air.local>
2025-10-27 01:05:07 +0200

    -review interface

---
+++ Repository/ReviewRepository.cs
@@ -0,0 +1,40 @@
+namespace dotnet.Repository
+{
+    using AutoMapper;
+    using dotnet.Data;
+    using dotnet.Interfaces;
+    using dotnet.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public Review GetReview(int reviewId)
+        {
+            return _context.Reviews.Where(r => r.Id == reviewId).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviews()
+        {
+            return _context.Reviews.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByBook(int bookId)
+        {
+            return _context.Reviews.Where(r => r.Book.Id == bookId).ToList();
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            return _context.Reviews.Any(r => r.Id == reviewId);
+        }
+    }
+}
